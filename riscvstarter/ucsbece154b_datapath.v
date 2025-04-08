1f2cba4ee8649a3a7c9d3fbf31c6577be3e5db9c
Justin Wu <justin_wu@ucsb.edu>
2025-04-08 11:46:23 -0700

    remove old code

--- riscvstarter/ucsbece154b_datapath.v
+++ riscvstarter/ucsbece154b_datapath.v
@@ -98,19 +98,6 @@ ucsbece154a_alu alu (
     .zero_o(ZeroE_o)
 );
 
-always @ * begin
-    case (ImmSrc_i) // add extra bit to support new immediate formats
-        imm_Itype: immExt = {{20{instr_i[31]}}, instr_i[31:20]};
-        imm_Stype: immExt = {{20{instr_i[31]}}, instr_i[31:25], instr_i[11:7]};
-        imm_Btype: immExt = {{19{instr_i[31]}}, instr_i[31], instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0};
-        imm_Jtype: immExt = {{12{instr_i[31]}}, instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0}; // imm format for j-type instr
-        imm_Utype: immExt = {instr_i[31:12], 12'b0}; // imm format for u-type instr
-        default: begin
-            immExt = 32'b0;
-        end
-    endcase
-end
-
 // Fetch-D pipeline register
 always @(posedge clk) begin
     if (reset || FlushD_i) begin
